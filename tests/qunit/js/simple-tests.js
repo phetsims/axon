df9bba669c64ccd373d94255936a8cb1bb629faa
Sam Reid <reids@colorado.edu>
2013-06-05 22:12:23 -0600

    added unit tests

--- tests/qunit/js/simple-tests.js
+++ tests/qunit/js/simple-tests.js
@@ -5,13 +5,26 @@
 
     var person = new axon.PropertySet( {name: 'larry', age: '100'} );
     equal( person.name, 'larry', 'name should be larry and accessible through es5 get' );
+
     person.name = 'susan';
     equal( person.name, 'susan', 'name should be susan after es5 set' );
+
     var ageLinkCalls = 0;
     person.ageProperty.link( function( age ) {ageLinkCalls++;} );
     person.age = person.age + 1;
     person.age = person.age + 2;
     person.age = person.age + 3;
     equal( ageLinkCalls, 4, 'should have received one call for each set, plus one on link' );
+
+    person.reset();
+    equal( person.name, 'larry', 'should have reset to the initial name' );
+
+    var myValue = '';
+    person.multilink( ['name', 'age'], function( name, age ) {
+      myValue = name + '/' + age;
+    } );
+    person.name = '123';
+    person.age = 456;
+    equal( myValue, '123/456', 'multilink should get both values' );
   } );
 })();
