118d90c059e9d031d9dd8f70486a1aa301f87074
Sam Reid <reids@colorado.edu>
2013-06-13 23:10:16 -0600

    added event channels to PropertySet

--- tests/qunit/js/simple-tests.js
+++ tests/qunit/js/simple-tests.js
@@ -49,5 +49,63 @@
       deepEqual( removed, [], 'Nothing removed' );
     } );
     array.add( 'd' );
-  } )
+  } );
+
+  test( 'Test events', function() {
+    var person = new axon.PropertySet( {name: 'larry', age: '100'} );
+    var count = 0;
+    var listener = function( person ) {
+      console.log( "resetted all" );
+      count = count + 1;
+    };
+    person.on( 'reset-all', listener );
+
+    person.trigger( 'reset-all' );
+    person.trigger( 'reset-all' );
+    person.trigger( 'reset-all' );
+
+    equal( count, 3, 'Trigger calls on' );
+
+    //Unregister the listener
+    person.off( 'reset-all', listener );
+
+    //Triggering more events shouldn't call back because we have removed the listener
+    person.trigger( 'reset-all' );
+    person.trigger( 'reset-all' );
+    person.trigger( 'reset-all' );
+
+    equal( count, 3, 'Triggering more events should not call back because we have removed the listener' );
+
+    var planetName = '?';
+    var planetRadius = '?';
+    person.on( 'planet-discovered', function( name, radius ) {
+      planetName = name;
+      planetRadius = radius;
+    } );
+
+    person.trigger( 'planet-discovered', 'pluto', 12345 );
+
+    equal( planetName, 'pluto', 'argument should pass through event' );
+    equal( planetRadius, 12345, 'argument should pass through event' );
+
+    var name = person.name;
+    person.once( 'name-changed', function( newName ) {
+      name = newName;
+    } );
+
+    person.trigger( 'name-changed', 'Alice' );
+    person.trigger( 'name-changed', 'Bob' );
+    person.trigger( 'name-changed', 'Charlie' );
+
+    equal( name, 'Alice', 'function added with once should only be called once' );
+
+    var x = 0;
+    var listener = function() {
+      x = 999;
+    };
+    var handle = person.once( 'say-hello', listener );
+    person.off( 'say-hello', handle );
+    person.trigger( 'say-hello' );
+    equal( x, 0, 'Function added with once should be removable' );
+  } );
 })();
