9213ace49dab8cb50e0244b7b913fcd515111a70
Chris Malley <cmalley@pixelzoom.com>
2015-04-09 11:16:21 -0600

    remove extra space from copyright comment on line 1 of .js files

--- js/ToggleProperty.js
+++ js/ToggleProperty.js
@@ -1,4 +1,4 @@
-//  Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2002-2014, University of Colorado Boulder
 
 /**
  * The ToggleProperty represents a Property.<boolean> mapped from another Property.<Object> that can take one of two
