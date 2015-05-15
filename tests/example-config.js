1199217e4b4a158afe5cf2fd59e5a7a087fd4179
Chris Malley <cmalley@pixelzoom.com>
2015-05-15 16:50:58 -0600

    move all 3rd-party libs to sherpa/lib/, phetsims/sherpa#17

--- tests/example-config.js
+++ tests/example-config.js
@@ -23,7 +23,7 @@ require.config( {
     audio: '../../chipper/js/requirejs-plugins/audio',
     string: '../../chipper/js/requirejs-plugins/string',
 
-    text: '../../sherpa/text-2.0.12'
+    text: '../../sherpa/lib/text-2.0.12'
   },
 
   // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
