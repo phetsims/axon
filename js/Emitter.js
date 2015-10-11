e2d1af978fdfafb49bd9668b99d244df92cb5f42
Sam Reid <reids@colorado.edu>
2015-10-10 23:54:30 -0600

    Factored out defendCallbacks, see #72

--- js/Emitter.js
+++ js/Emitter.js
@@ -32,24 +32,51 @@ define( function( require ) {
   axon.Emitter = Emitter;
 
   return inherit( Object, Emitter, {
+
+    /**
+     * @public add a listener
+     * @param listener
+     */
     addListener: function( listener ) {
+
+      // If callbacks are in progress, make a copy of the current list of listeners--the newly added listener
+      // will be available for the next emit() but not the one in progress.  This is to match behavior with removeListener
+      this.defendCallbacks();
+
       this.listeners.push( listener );
     },
+
+    /**
+     * @public remove a listener
+     * @param listener
+     */
     removeListener: function( listener ) {
 
       var index = this.listeners.indexOf( listener );
       assert && assert( index >= 0, 'tried to removeListener on something that wasnt a listener' );
 
-      // If processing callbacks during an emit() call and removeListener() is called,
-      // make a defensive copy of the array of listener before removing anything, and use it for 
-      // the rest of the callbacks.
+      // If callbacks are in progress, make a copy of the current list of listeners--the removed listener 
+      // will remain in the list and receive a callback for this emit call, see #72
+      this.defendCallbacks();
+
+      this.listeners.splice( index, 1 );
+    },
+
+    /**
+     * @private - If processing callbacks during an emit() call and addListener/removeListener() is called,
+     * make a defensive copy of the array of listener before changing the array, and use it for
+     * the rest of the callbacks until the emit call has completed.
+     */
+    defendCallbacks: function() {
       if ( this.listenersToEmitTo !== null && !this.alreadyCopiedDuringEmit ) {
         this.listenersToEmitTo = this.listeners.slice();
         this.alreadyCopiedDuringEmit = true;
       }
-
-      this.listeners.splice( index, 1 );
     },
+
+    /**
+     * Remove all the listeners
+     */
     removeAllListeners: function() {
       while ( this.listeners.length > 0 ) {
         this.removeListener( this.listeners[ 0 ] );
@@ -70,6 +97,10 @@ define( function( require ) {
       this.listenersToEmitTo = null;
       this.alreadyCopiedDuringEmit = false;
     },
+    /**
+     * emit a single event with one argument.  This is a copy-paste of emit() for performance reasons.
+     * @param arg1
+     */
     emit1: function( arg1 ) {
       assert && assert( this.listenersToEmitTo === null, 're-entrant emit not allowed because it would interfere with removal of ' +
                                                          'listeners while processing emit' );
@@ -81,6 +112,11 @@ define( function( require ) {
       this.listenersToEmitTo = null;
       this.alreadyCopiedDuringEmit = false;
     },
+    /**
+     * emit a single event with two arguments.  This is a copy-paste of emit() for performance reasons.
+     * @param arg1
+     * @param arg2
+     */
     emit2: function( arg1, arg2 ) {
       assert && assert( this.listenersToEmitTo === null, 're-entrant emit not allowed because it would interfere with removal of ' +
                                                          'listeners while processing emit' );
@@ -92,6 +128,12 @@ define( function( require ) {
       this.listenersToEmitTo = null;
       this.alreadyCopiedDuringEmit = false;
     },
+
+    /**
+     * @public - check whether the listener is registered with this Emitter
+     * @param listener
+     * @returns {boolean}
+     */
     containsListener: function( listener ) {
       return this.listeners.indexOf( listener ) >= 0;
     }
