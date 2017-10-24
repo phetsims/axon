80b602933d64501d55105dbc538a6ff83d019fec
zepumph <michael.kauzmann@colorado.edu>
2017-10-24 15:42:52 -0800

    Remove TODO, see https://github.com/phetsims/axon/issues/158

--- js/NumberProperty.js
+++ js/NumberProperty.js
@@ -18,7 +18,6 @@ define( function( require ) {
   // phet-io modules
   var TNumber = require( 'ifphetio!PHET_IO/types/TNumber' );
 
-  // TODO: Should this be an enum?  @zepumph says probably. See https://github.com/phetsims/axon/issues/158
   // valid values for options.units
   var VALID_UNITS = [
     'amperes',
