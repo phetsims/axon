37d5839cf25343fec5811fefd088eeb510b8950b
Jonathan Olson <jonathan.olson@colorado.edu>
2018-05-29 15:06:47 -0600

    Adding window.phetAllocation to fix tests (so we have a global). See https://github.com/phetsims/phet-core/issues/38

--- js/axon.js
+++ js/axon.js
@@ -5,5 +5,8 @@ define( function( require ) {
 
   var Namespace = require( 'PHET_CORE/Namespace' );
 
+  // Object allocation tracking - Set here so we can use a global reference that gets stripped out by uglify.
+  window.phetAllocation = require( 'PHET_CORE/phetAllocation' );
+
   return new Namespace( 'axon' );
 } );
