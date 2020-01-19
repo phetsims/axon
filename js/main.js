088afb20c59030995200ca3a2c829bb243281e26
samreid <reids@colorado.edu>
2020-01-18 20:54:24 -0700

    Disable lint, see https://github.com/phetsims/chipper/issues/820

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 // Copyright 2013-2019, University of Colorado Boulder
 
+// load modules so they will attach to the namespace
 define( require => {
   'use strict';
 
   // modules
-  const axon = require( 'AXON/axon' );
-  const BooleanProperty = require( 'AXON/BooleanProperty' );
-  const DerivedProperty = require( 'AXON/DerivedProperty' );
-  const DynamicProperty = require( 'AXON/DynamicProperty' );
-  const Emitter = require( 'AXON/Emitter' );
-  const Events = require( 'AXON/Events' );
-  const Multilink = require( 'AXON/Multilink' );
-  const ObservableArray = require( 'AXON/ObservableArray' );
-  const Property = require( 'AXON/Property' );
-  const StringProperty = require( 'AXON/StringProperty' );
-  const NumberProperty = require( 'AXON/NumberProperty' );
+  const axon = require( 'AXON/axon' ); // eslint-disable-line
+  const BooleanProperty = require( 'AXON/BooleanProperty' ); // eslint-disable-line
+  const DerivedProperty = require( 'AXON/DerivedProperty' ); // eslint-disable-line
+  const DynamicProperty = require( 'AXON/DynamicProperty' ); // eslint-disable-line
+  const Emitter = require( 'AXON/Emitter' );// eslint-disable-line
+  const Events = require( 'AXON/Events' );// eslint-disable-line
+  const Multilink = require( 'AXON/Multilink' );// eslint-disable-line
+  const ObservableArray = require( 'AXON/ObservableArray' );// eslint-disable-line
+  const Property = require( 'AXON/Property' );// eslint-disable-line
+  const StringProperty = require( 'AXON/StringProperty' );// eslint-disable-line
+  const NumberProperty = require( 'AXON/NumberProperty' );// eslint-disable-line
 
   return axon;
 } );
