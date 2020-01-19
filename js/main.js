69bcbe4d33cc91195caa20910c4c57b91802252b
samreid <reids@colorado.edu>
2020-01-18 20:51:06 -0700

    Standardize files for migration, see https://github.com/phetsims/chipper/issues/820

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 // Copyright 2013-2019, University of Colorado Boulder
 
-define( [
-  'AXON/axon',
-  'AXON/BooleanProperty',
-  'AXON/DerivedProperty',
-  'AXON/DynamicProperty',
-  'AXON/Emitter',
-  'AXON/Events',
-  'AXON/Multilink',
-  'AXON/ObservableArray',
-  'AXON/Property',
-  'AXON/StringProperty',
-  'AXON/NumberProperty'
-], function( axon ) {
+define( require => {
   'use strict';
+
+  // modules
+  const axon = require( 'AXON/axon' );
+  const BooleanProperty = require( 'AXON/BooleanProperty' );
+  const DerivedProperty = require( 'AXON/DerivedProperty' );
+  const DynamicProperty = require( 'AXON/DynamicProperty' );
+  const Emitter = require( 'AXON/Emitter' );
+  const Events = require( 'AXON/Events' );
+  const Multilink = require( 'AXON/Multilink' );
+  const ObservableArray = require( 'AXON/ObservableArray' );
+  const Property = require( 'AXON/Property' );
+  const StringProperty = require( 'AXON/StringProperty' );
+  const NumberProperty = require( 'AXON/NumberProperty' );
+
   return axon;
 } );
