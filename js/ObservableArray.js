b945f7de788208dc128bd27d29c07f4b2cc29a3b
zepumph <michael.kauzmann@colorado.edu>
2020-05-13 14:41:51 -0800

    convert to es6 class, https://github.com/phetsims/axon/issues/270

--- js/ObservableArray.js
+++ js/ObservableArray.js
@@ -10,7 +10,6 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import PhetioObject from '../../tandem/js/PhetioObject.js';
 import Tandem from '../../tandem/js/Tandem.js';
@@ -23,52 +22,51 @@ import ObservableArrayIO from './ObservableArrayIO.js';
 // Factor out to reduce memory footprint, see https://github.com/phetsims/tandem/issues/71
 const DefaultObservableArrayIOType = ObservableArrayIO( ObjectIO );
 
-/**
- * @param {Object} [options]
- * @constructor
- */
-function ObservableArray( options ) {
-  assert && assert( !Array.isArray( options ), 'ObservableArray cannot be initialized with values' );
+class ObservableArray extends PhetioObject {
 
-  options = merge( {
-    allowDuplicates: false, // are duplicate items allowed in the array?
-    phetioType: DefaultObservableArrayIOType,
-    tandem: Tandem.OPTIONAL
-  }, options );
+  /**
+   * @param {Object} [options]
+   */
+  constructor( options ) {
+    assert && assert( !Array.isArray( options ), 'ObservableArray cannot be initialized with values' );
 
-  assert && assert( options.phetioType && options.phetioType.parameterTypes, 'ObservableArray\'s phetioType should have a parameterType' );
-  assert && assert( options.phetioType && options.phetioType.parameterTypes.length === 1, 'ObservableArray\'s phetioType should have a parameterType' );
-  assert && assert( options.phetioType && options.phetioType.parameterTypes[ 0 ], 'ObservableArray\'s phetioType should have a parameterType' );
+    options = merge( {
+      allowDuplicates: false, // are duplicate items allowed in the array?
+      phetioType: DefaultObservableArrayIOType,
+      tandem: Tandem.OPTIONAL
+    }, options );
 
-  this.allowDuplicates = options.allowDuplicates; // @private
+    assert && assert( options.phetioType && options.phetioType.parameterTypes, 'ObservableArray\'s phetioType should have a parameterType' );
+    assert && assert( options.phetioType && options.phetioType.parameterTypes.length === 1, 'ObservableArray\'s phetioType should have a parameterType' );
+    assert && assert( options.phetioType && options.phetioType.parameterTypes[ 0 ], 'ObservableArray\'s phetioType should have a parameterType' );
 
-  this._options = options; // @private, for creating internal ObservableArray copies, see map/filter
+    super( options );
 
-  this._array = []; // @private internal, do not access directly
+    this.allowDuplicates = options.allowDuplicates; // @private
 
-  // @private
-  this.itemAddedEmitter = new Emitter( { parameters: [ { isValidValue: _.stubTrue }, { valueType: ObservableArray } ] } );
-  this.itemRemovedEmitter = new Emitter( { parameters: [ { isValidValue: _.stubTrue }, { valueType: ObservableArray } ] } );
+    this._options = options; // @private, for creating internal ObservableArray copies, see map/filter
 
-  // @public (read-only) observe this, but don't set it
-  this.lengthProperty = new NumberProperty( this._array.length, {
-    numberType: 'Integer',
-    tandem: options.tandem.createTandem( 'lengthProperty' ),
-    phetioReadOnly: true
-  } );
+    this._array = []; // @private internal, do not access directly
 
-  PhetioObject.call( this, options );
-}
+    // @private
+    this.itemAddedEmitter = new Emitter( { parameters: [ { isValidValue: _.stubTrue }, { valueType: ObservableArray } ] } );
+    this.itemRemovedEmitter = new Emitter( { parameters: [ { isValidValue: _.stubTrue }, { valueType: ObservableArray } ] } );
 
-axon.register( 'ObservableArray', ObservableArray );
+    // @public (read-only) observe this, but don't set it
+    this.lengthProperty = new NumberProperty( this._array.length, {
+      numberType: 'Integer',
+      tandem: options.tandem.createTandem( 'lengthProperty' ),
+      phetioReadOnly: true
+    } );
+  }
 
-inherit( PhetioObject, ObservableArray, {
-
-  // @public
-  dispose: function() {
+  /**
+   * @public
+   */
+  dispose() {
     this.lengthProperty.dispose();
     PhetioObject.prototype.dispose.call( this );
-  },
+  }
 
   /**
    * Restore the array back to its initial state
@@ -76,59 +74,59 @@ inherit( PhetioObject, ObservableArray, {
    * This may or may not change in the future, see #4
    * @public
    */
-  reset: function() {
+  reset() {
     this.clear();
-  },
+  }
 
   // @public
-  get length() { return this._array.length; },
+  get length() { return this._array.length; }
 
   /**
    * Adds a listener that will be notified when an item is added to the list.
    * @param {function(item:*, ObservableArray)} listener
    * @public
    */
-  addItemAddedListener: function( listener ) {
+  addItemAddedListener( listener ) {
     assert && assert( !this.itemAddedEmitter.hasListener( listener ), 'listener is already registered' );
     this.itemAddedEmitter.addListener( listener );
-  },
+  }
 
   /**
    * Removes a listener that was added via addItemAddedListener.
    * @param {function(item:*, ObservableArray)} listener
    * @public
    */
-  removeItemAddedListener: function( listener ) {
+  removeItemAddedListener( listener ) {
     assert && assert( this.itemAddedEmitter.hasListener( listener ), 'listener is not registered' );
     this.itemAddedEmitter.removeListener( listener );
-  },
+  }
 
   /**
    * Adds a listener that will be notified when an item is removed from the list.
    * @param listener function( item, observableArray )
    * @public
    */
-  addItemRemovedListener: function( listener ) {
+  addItemRemovedListener( listener ) {
     assert && assert( !this.itemRemovedEmitter.hasListener( listener ), 'listener is already registered' );
     this.itemRemovedEmitter.addListener( listener );
-  },
+  }
 
   /**
    * Removes a listener that was added via addItemRemovedListener.
    * @param listener
    * @public
    */
-  removeItemRemovedListener: function( listener ) {
+  removeItemRemovedListener( listener ) {
     assert && assert( this.itemRemovedEmitter.hasListener( listener ), 'listener is not registered' );
     this.itemRemovedEmitter.removeListener( listener );
-  },
+  }
 
   /**
    * called when an item is added
    * @param {*} item
    * @private
    */
-  _fireItemAdded: function( item ) {
+  _fireItemAdded( item ) {
     this.phetioStartEvent( 'itemAdded', {
       getData: () => {
         return this.phetioType.parameterTypes[ 0 ].toStateObject( item );
@@ -138,14 +136,14 @@ inherit( PhetioObject, ObservableArray, {
     this.itemAddedEmitter.emit( item, this );
 
     this.phetioEndEvent();
-  },
+  }
 
   /**
    * called when an item is added
    * @param {*} item
    * @private
    */
-  _fireItemRemoved: function( item ) {
+  _fireItemRemoved( item ) {
     this.phetioStartEvent( 'itemRemoved', {
       getData: () => {
         return this.phetioType.parameterTypes[ 0 ].toStateObject( item );
@@ -155,7 +153,7 @@ inherit( PhetioObject, ObservableArray, {
     this.itemRemovedEmitter.emit( item, this );
 
     this.phetioEndEvent();
-  },
+  }
 
   /**
    * Adds an item to the end of the array.
@@ -163,9 +161,9 @@ inherit( PhetioObject, ObservableArray, {
    * @param item
    * @public
    */
-  add: function( item ) {
+  add( item ) {
     this.push( item );
-  },
+  }
 
   /**
    * Add items to the end of the array.
@@ -173,11 +171,11 @@ inherit( PhetioObject, ObservableArray, {
    * @param {Array} items
    * @public
    */
-  addAll: function( items ) {
+  addAll( items ) {
     for ( let i = 0; i < items.length; i++ ) {
       this.add( items[ i ] );
     }
-  },
+  }
 
   /**
    * Removes the first occurrence of an item from the array.
@@ -186,14 +184,14 @@ inherit( PhetioObject, ObservableArray, {
    * @param item
    * @public
    */
-  remove: function( item ) {
+  remove( item ) {
     const index = this._array.indexOf( item );
     if ( index !== -1 ) {
       this._array.splice( index, 1 );
       this.lengthProperty.set( this._array.length );
       this._fireItemRemoved( item );
     }
-  },
+  }
 
   /**
    * Removes the first occurrence of each item in the specified array.
@@ -201,12 +199,12 @@ inherit( PhetioObject, ObservableArray, {
    * @see ObservableArray.remove
    * @public
    */
-  removeAll: function( array ) {
+  removeAll( array ) {
     assert && assert( _.isArray( array ), 'array should be an array' );
     for ( let i = 0; i < array.length; i++ ) {
       this.remove( array[ i ] );
     }
-  },
+  }
 
   /**
    * Pushes an item onto the end of the array.
@@ -214,21 +212,21 @@ inherit( PhetioObject, ObservableArray, {
    * @throws Error if duplicates are not allowed (see options.allowDuplicates) and item is already in the array
    * @public
    */
-  push: function( item ) {
+  push( item ) {
     if ( !this.allowDuplicates && this.contains( item ) ) {
       throw new Error( 'duplicates are not allowed' );
     }
     this._array.push( item );
     this.lengthProperty.set( this._array.length );
     this._fireItemAdded( item );
-  },
+  }
 
   /**
    * Removes an item from the end of the array and returns it.
    * @returns {*}
    * @public
    */
-  pop: function() {
+  pop() {
     assert && assert( this.length > 0, 'cannot pop an empty ObservableArray' );
 
     const item = this._array.pop();
@@ -237,14 +235,14 @@ inherit( PhetioObject, ObservableArray, {
       this._fireItemRemoved( item );
     }
     return item;
-  },
+  }
 
   /**
    * Removes an item from the beginning of the array and returns it.
    * @returns {*}
    * @public
    */
-  shift: function() {
+  shift() {
     assert && assert( this.length > 0, 'cannot shift an empty ObservableArray' );
 
     const item = this._array.shift();
@@ -253,7 +251,7 @@ inherit( PhetioObject, ObservableArray, {
       this._fireItemRemoved( item );
     }
     return item;
-  },
+  }
 
   /**
    * Does the array contain the specified item?
@@ -261,9 +259,9 @@ inherit( PhetioObject, ObservableArray, {
    * @returns {boolean}
    * @public
    */
-  contains: function( item ) {
+  contains( item ) {
     return this.indexOf( item ) !== -1;
-  },
+  }
 
   /**
    * Gets an item at the specified index.
@@ -272,10 +270,10 @@ inherit( PhetioObject, ObservableArray, {
    * @throws assertion Error if the index is out of bounds
    * @public
    */
-  get: function( index ) {
+  get( index ) {
     assert && assert( index >= 0 && index < this.length, `index out of bounds: ${index}` );
     return this._array[ index ];
-  },
+  }
 
   /**
    * Gets the index of a specified item.
@@ -283,30 +281,30 @@ inherit( PhetioObject, ObservableArray, {
    * @returns {*} -1 if item is not in the array
    * @public
    */
-  indexOf: function( item ) {
+  indexOf( item ) {
     return this._array.indexOf( item );
-  },
+  }
 
   /**
    * Removes all items from the array.
    * @public
    */
-  clear: function() {
+  clear() {
     while ( this.length > 0 ) {
       this.pop();
     }
-  },
+  }
 
   /**
    * Applies a callback function to each item in the array
    * @param {function(item:*)} callback
    * @public
    */
-  forEach: function( callback ) {
+  forEach( callback ) {
 
     // TODO: don't slice on forEach, see https://github.com/phetsims/axon/issues/283
     this._array.slice().forEach( callback ); // do this on a copy of the array, in case callbacks involve array modification
-  },
+  }
 
   /**
    * Maps the values in this ObservableArray using the specified function, and returns a new ObservableArray for chaining.
@@ -314,9 +312,9 @@ inherit( PhetioObject, ObservableArray, {
    * @returns {Object[]}
    * @public
    */
-  map: function( mapFunction ) {
+  map( mapFunction ) {
     return this._array.map( mapFunction );
-  },
+  }
 
   /**
    * Filters the values in this ObservableArray using the predicate function, and returns a new ObservableArray for chaining.
@@ -324,9 +322,9 @@ inherit( PhetioObject, ObservableArray, {
    * @returns {Object[]}
    * @public
    */
-  filter: function( predicate ) {
+  filter( predicate ) {
     return this._array.filter( predicate );
-  },
+  }
 
   /**
    * Count the number of items in this ObservableArray that satisfy the given Predicate.
@@ -334,7 +332,7 @@ inherit( PhetioObject, ObservableArray, {
    * @returns {number}
    * @public
    */
-  count: function( predicate ) {
+  count( predicate ) {
     let count = 0;
     for ( let i = 0; i < this._array.length; i++ ) {
       if ( predicate( this._array[ i ] ) ) {
@@ -342,7 +340,7 @@ inherit( PhetioObject, ObservableArray, {
       }
     }
     return count;
-  },
+  }
 
   /**
    * Find the first element that matches the given predicate.
@@ -352,21 +350,21 @@ inherit( PhetioObject, ObservableArray, {
    * @throws assertion Error if the fromIndex is specified and out of range
    * @public
    */
-  find: function( predicate, fromIndex ) {
+  find( predicate, fromIndex ) {
     assert && ( fromIndex !== undefined ) && assert( typeof fromIndex === 'number', 'fromIndex must be numeric, if provided' );
     assert && ( typeof fromIndex === 'number' ) && assert( fromIndex >= 0 && fromIndex < this.length,
       `fromIndex out of bounds: ${fromIndex}` );
     return _.find( this._array, predicate, fromIndex );
-  },
+  }
 
   /**
    * Returns true if some element in this ObservableArray matches the predicate.
    * @param {function(item:*):boolean} predicate
    * @returns {boolean}
    */
-  some: function( predicate ) {
+  some( predicate ) {
     return _.some( this._array, predicate );
-  },
+  }
 
   /**
    * Starting with the initial value, combine values from this ObservableArray to come up with a composite result.
@@ -376,12 +374,12 @@ inherit( PhetioObject, ObservableArray, {
    * @returns {*}
    * @public
    */
-  reduce: function( value, combiner ) {
+  reduce( value, combiner ) {
     for ( let i = 0; i < this._array.length; i++ ) {
       value = combiner( value, this._array[ i ] );
     }
     return value;
-  },
+  }
 
   /**
    * Return the underlying array.  Most clients should be able to use the ObservableArray functions for array access
@@ -391,18 +389,18 @@ inherit( PhetioObject, ObservableArray, {
    * @returns {Array}
    * @public
    */
-  getArray: function() {
+  getArray() {
     return this._array;
-  },
+  }
 
   /**
    * Like getArray, but returns a copy of the array. Use this if you will be modifying the collection.
    * @returns {Array}
    * @public
    */
-  getArrayCopy: function() {
+  getArrayCopy() {
     return this._array.slice();
-  },
+  }
 
   /**
    * Add/remove elements from any point in the array
@@ -414,7 +412,7 @@ inherit( PhetioObject, ObservableArray, {
    * @returns {Object[]} the items that were deleted.
    * @public
    */
-  splice: function( start, deleteCount, item1, item2, etc ) {
+  splice( start, deleteCount, item1, item2, etc ) {
     const deleted = this._array.splice.apply( this._array, arguments );
     const args = Array.prototype.slice.call( arguments );
     for ( let i = 0; i < deleted.length; i++ ) {
@@ -425,14 +423,14 @@ inherit( PhetioObject, ObservableArray, {
       this._fireItemAdded( args[ k ] );
     }
     return deleted;
-  },
+  }
 
   /**
    * Changes the ordering of elements in the array.  Requires a Random source so that shuffles can be reproducible.
    * No items are added or removed, and this method does not send out any notifications.
    * @param {Random} random - from dot
    */
-  shuffle: function( random ) {
+  shuffle( random ) {
     assert && assert( random, 'random must be supplied' );
 
     // preserve the same _array reference in case any clients got a reference to it with getArray()
@@ -440,6 +438,7 @@ inherit( PhetioObject, ObservableArray, {
     this._array.length = 0;
     Array.prototype.push.apply( this._array, shuffled );
   }
-} );
+}
 
+axon.register( 'ObservableArray', ObservableArray );
 export default ObservableArray;
