3abdd749d6e8abce4876182ae9bde8d50167a49f
Jonathan Olson <jonathan.olson@colorado.edu>
2020-06-02 10:40:40 -0600

    Adding back in axon's main.js content, see https://github.com/phetsims/scenery/issues/490

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
 // Copyright 2013-2020, University of Colorado Boulder
 
-// load modules so they will attach to the namespace
+/**
+ * Module that includes all axon dependencies, so that requiring this module will return an object
+ * that consists of the entire exported 'axon' namespace API.
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
 
-
-import axon from './axon.js'; // eslint-disable-line
+import './Action.js';
+import './BooleanProperty.js';
+import './CallbackTimer.js';
+import './DerivedProperty.js';
+import './DynamicProperty.js';
+import './Emitter.js';
+import './EnumerationProperty.js';
+import './Multilink.js';
+import './NumberProperty.js';
+import './ObservableArray.js';
+import './Property.js';
+import './PropertyStateHandler.js';
+import './PropertyStatePhase.js';
+import './StringProperty.js';
+import './TinyEmitter.js';
+import './TinyForwardingProperty.js';
+import './TinyProperty.js';
+import './TinyStaticProperty.js';
+import './ValidatorDef.js';
+import axon from './axon.js';
+import './propertyStateHandlerSingleton.js';
+import './timer.js';
+import './units.js';
+import './validate.js';
 
 export default axon;
