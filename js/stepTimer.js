09f16b8e072d9a5d05997ad860640c3085017209
samreid <reids@colorado.edu>
2020-09-11 20:01:53 -0600

    Update JSDoc, see https://github.com/phetsims/axon/issues/329

--- js/stepTimer.js
+++ js/stepTimer.js
@@ -4,8 +4,12 @@ import Timer from './Timer.js';
 import axon from './axon.js';
 
 /**
- * Register and return a singleton timer. Only ticks when the sim is active, during the step. For a timer that runs even
- * when the sim is inactive and not stepping, see animationFrameTimer.
+ * Register and return a singleton timer, which can be used to schedule events. This ticks under the following circumstances:
+ * 1) When a JOIST/Sim is active, during the step event
+ * 2) In various accessibility tests that require timing
+ * 3) In SCENERY/Display's animation loop, which is run by various test and example HTML files
+ *
+ * In order to run an event when a simulation is inactive and not stepping, see animationFrameTimer.
  *
  * Listeners added with addListener are called with a {number} dt argument (in seconds) via timer.emit in
  * Sim.stepSimulation. Listeners added with setTimeout/setInterval are called with no arguments. This is not specific
