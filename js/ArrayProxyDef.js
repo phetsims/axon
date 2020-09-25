98b3605846736b1ae889dcd45401752d9694d1f6
samreid <reids@colorado.edu>
2020-09-25 09:01:20 -0600

    Add ArrayProxyDef, see https://github.com/phetsims/axon/issues/330

---
+++ js/ArrayProxyDef.js
@@ -0,0 +1,31 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * This is a type definition for values returned by createArrayProxy.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+import axon from './axon.js';
+
+/**
+ * @typedef {Object} ArrayProxyDef
+ * See createArrayProxy for details
+ * @property {Emitter} elementAddedEmitter
+ * @property {Emitter} elementRemovedEmitter
+ * @property {Property<Number>} lengthProperty
+ */
+const ArrayProxyDef = {
+
+  /**
+   * Returns true if the argument has the properties that an Array Proxy should have.
+   * @public
+   */
+  isArrayProxy( arrayProxy ) {
+
+    return Array.isArray( arrayProxy ) && arrayProxy.elementAddedEmitter && arrayProxy.elementRemovedEmitter && arrayProxy.lengthProperty;
+  }
+};
+
+axon.register( 'ArrayProxyDef', ArrayProxyDef );
+export default ArrayProxyDef;
