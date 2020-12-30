c6d6d8b94e613d3093a98974b2382a3b293d0283
zepumph <michael.kauzmann@colorado.edu>
2020-12-30 14:59:10 -0900

    doc and spacing

--- js/animationFrameTimer.js
+++ js/animationFrameTimer.js
@@ -1,9 +1,9 @@
 // Copyright 2020, University of Colorado Boulder
 
-import Timer from './Timer.js';
 import axon from './axon.js';
+import Timer from './Timer.js';
 
-// Like timer but runs every frame whether the sim is active or not.
+// Like stepTimer but runs every frame whether the sim is active or not.
 const animationFrameTimer = new Timer( { parameters: [ { valueType: 'number' } ] } );
 
 axon.register( 'animationFrameTimer', animationFrameTimer );
