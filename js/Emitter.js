d848101bd6e9f29d49173b5f41e8a5c7baa70516
phet-dev <phetadmin@gmail.com>
2021-05-20 14:56:10 -0600

    update copyright dates from daily grunt work

--- js/Emitter.js
+++ js/Emitter.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * Event & listener abstraction for a single "event" type. The type provides extra functionality beyond just notifying
--- js/ValidatorDef.js
+++ js/ValidatorDef.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * The definition file for "validators" used to validate values. This file holds associated logic that validates the
