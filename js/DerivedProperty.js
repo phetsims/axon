fa529a848da800183b03818033ff4027658bfaea
samreid <reids@colorado.edu>
2021-07-30 21:42:04 -0600

    Match return type with parent type, see https://github.com/phetsims/gravity-and-orbits/issues/405

--- js/DerivedProperty.js
+++ js/DerivedProperty.js
@@ -135,7 +135,12 @@ class DerivedProperty extends Property {
    * @override
    * @public
    */
-  set( value ) { throw new Error( `Cannot set values directly to a DerivedProperty, tried to set: ${value}` ); }
+  set( value ) {
+    if ( true ) { // eslint-disable-line
+      throw new Error( `Cannot set values directly to a DerivedProperty, tried to set: ${value}` );
+    }
+    return this;
+  }
 
   /**
    * Override the mutators to provide an error message.  These should not be called directly, the value should only be modified
