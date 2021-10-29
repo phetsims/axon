1116c3dd29e7d529d5f7fa2c8ae6df7536b7f853
Sam Reid <reids@colorado.edu>
2021-10-28 21:24:13 -0600

    Add phet-io instrumentation and update enumeration IO type, https://github.com/phetsims/chipper/issues/1106

--- js/wilder/model/RichEnumerationProperty.ts
+++ js/wilder/model/RichEnumerationProperty.ts
@@ -17,13 +17,23 @@ const cache = new Map();
 /**
  * This caching implementation should be kept in sync with the other parametric IO Type caching implementations.
  */
-const RichEnumuerationIO = <T>( enumeration: T, keys: string[], values: T[] ): IOType => {
+const RichEnumuerationIO = <T>( enumeration: any ): IOType => {
 
   if ( !cache.has( enumeration ) ) {
-    const getKey = ( value: T ) => keys.find( key =>
 
-      // @ts-ignore
-      enumeration[ key ] === value );
+    const keyToValueMap = new Map<string, T>();
+    const valueToKeyMap = new Map<T, string>();
+
+    for ( const key in enumeration ) {
+      const value = enumeration[ key ];
+      if ( value instanceof enumeration ) {
+        keyToValueMap.set( key, value );
+        valueToKeyMap.set( value, key );
+      }
+    }
+
+    const keys = Array.from( keyToValueMap.keys() );
+    const values = Array.from( keyToValueMap.values() );
 
     // Enumeration supports additional documentation, so the values can be described.
     const additionalDocs = 'docs';//enumeration.phetioDocumentation ? ` ${enumeration.phetioDocumentation}` : '';
@@ -31,13 +41,11 @@ const RichEnumuerationIO = <T>( enumeration: T, keys: string[], values: T[] ): I
     cache.set( enumeration, new IOType( `RichEnumuerationIO(${keys.join( '|' )})`, {
       validValues: values,
       documentation: `Possible values: ${keys.join( ', ' )}.${additionalDocs}`,
-      toStateObject: getKey,
+      toStateObject: ( t: T ) => valueToKeyMap.get( t ),
       fromStateObject: ( stateObject: string ) => {
         assert && assert( typeof stateObject === 'string', 'unsupported RichEnumuerationIO value type, expected string' );
         assert && assert( keys.indexOf( stateObject ) >= 0, `Unrecognized value: ${stateObject}` );
-
-        // @ts-ignore
-        return enumeration[ stateObject ];
+        return keyToValueMap.get( stateObject )!;
       },
       stateSchema: StateSchema.asValue( `${keys.join( '|' )}`, {
         isValidValue: ( v: string ) => keys.includes( v )
@@ -51,15 +59,8 @@ const RichEnumuerationIO = <T>( enumeration: T, keys: string[], values: T[] ): I
 class RichEnumerationProperty<T> extends Property<T> {
   constructor( EnumerationType: any, value: T, providedOptions?: any ) {
     providedOptions = providedOptions || {};
-    const validValues = [];
-    const keys = [];
-    for ( const property in EnumerationType ) {
-      if ( EnumerationType[ property ] instanceof EnumerationType ) {
-        validValues.push( EnumerationType[ property ] );
-        keys.push( property );
-      }
-    }
-    providedOptions.phetioType = Property.PropertyIO( RichEnumuerationIO<T>( EnumerationType, keys, validValues ) );
+    providedOptions.validValues = _.values( EnumerationType ).filter( value => value instanceof EnumerationType );
+    providedOptions.phetioType = Property.PropertyIO( RichEnumuerationIO<T>( EnumerationType ) );
     super( value, providedOptions );
   }
 }
