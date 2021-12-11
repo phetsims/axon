59b74d5b7583e429616fd3cc20f76f9c9f7d8176
Sam Reid <reids@colorado.edu>
2021-12-10 19:51:46 -0700

    Documentation, and renaming, minor refactoring, see https://github.com/phetsims/chipper/issues/1106

--- js/wilder/model/RichEnumerationProperty.ts
+++ js/wilder/model/RichEnumerationProperty.ts
@@ -1,32 +1,55 @@
 // Copyright 2021, University of Colorado Boulder
 
 /**
- * IO Type for phet-core Enumeration that supports serializing and deserializing values. Cannot be moved to the core
- * type since Enumeration must be defined before ValidatorDef can be defined.
+ * Property support for rich enumeration types. Please see Orientation.ts for an example. This implementation
+ * auto-detects the enumeration values by Object.keys and instanceof.  Every property that has a type matching
+ * the enumeration type is marked as a value.
+ *
+ * class T{
+ *     static a=new T();
+ *     static b =new T();
+ *     constructor(){}
+ *     getName(){return 'he';}
+ *     get thing(){return 'text';}
+ *     static get age(){return 77;}
+ * }
+ * Object.keys(T) => ['a', 'b']
+ * Object.values(T) => [T, T]
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-import Property from '../../../../axon/js/Property.js';
+import Property, { PropertyOptions } from '../../../../axon/js/Property.js';
 import IOType from '../../../../tandem/js/types/IOType.js';
 import StateSchema from '../../../../tandem/js/types/StateSchema.js';
 
-// {Map.<enumeration:Enumeration, IOType>} - Cache each parameterized RichEnumerationIO so that it is only created once.
-const cache = new Map();
+// Cannot model as Constructor = new (...args) because we want to have private constructors
+type RichEnumeration = Function & { [ key: string ]: any };
 
-/**
- * This caching implementation should be kept in sync with the other parametric IO Type caching implementations.
- */
-const RichEnumerationIO = <T>( enumeration: any ): IOType => {
+class RichEnumerationProperty<T> extends Property<T> {
+  constructor( Enumeration: RichEnumeration, value: T, providedOptions?: PropertyOptions<T> ) {
+    providedOptions = providedOptions || {};
+    providedOptions.validValues = Object.values( Enumeration ).filter( value => value instanceof Enumeration );
+    providedOptions.phetioType = Property.PropertyIO( RichEnumerationIO<T>( Enumeration ) );
+    super( value, providedOptions );
+  }
+}
 
-  if ( !cache.has( enumeration ) ) {
+// Cache each parameterized IOType so that it is only created once.
+const cache = new Map<RichEnumeration, IOType>();
 
+const RichEnumerationIO = <T>( Enumeration: RichEnumeration ): IOType => {
+
+  // This caching implementation should be kept in sync with the other parametric IO Type caching implementations.
+  if ( !cache.has( Enumeration ) ) {
+
+    // This work is only done once per RichEnumeration
     const keyToValueMap = new Map<string, T>();
     const valueToKeyMap = new Map<T, string>();
 
-    for ( const key in enumeration ) {
-      const value = enumeration[ key ];
-      if ( value instanceof enumeration ) {
+    for ( const key in Enumeration ) {
+      const value = Enumeration[ key ];
+      if ( value instanceof Enumeration ) {
         keyToValueMap.set( key, value );
         valueToKeyMap.set( value, key );
       }
@@ -38,13 +61,13 @@ const RichEnumerationIO = <T>( enumeration: any ): IOType => {
     // Enumeration supports additional documentation, so the values can be described.
     const additionalDocs = 'docs';//enumeration.phetioDocumentation ? ` ${enumeration.phetioDocumentation}` : '';
 
-    cache.set( enumeration, new IOType( `RichEnumerationIO(${keys.join( '|' )})`, {
+    cache.set( Enumeration, new IOType( `RichEnumerationIO(${keys.join( '|' )})`, {
       validValues: values,
       documentation: `Possible values: ${keys.join( ', ' )}.${additionalDocs}`,
       toStateObject: ( t: T ) => valueToKeyMap.get( t ),
-      fromStateObject: ( stateObject: string ) => {
+      fromStateObject: ( stateObject: string ): T => {
         assert && assert( typeof stateObject === 'string', 'unsupported RichEnumerationIO value type, expected string' );
-        assert && assert( keys.indexOf( stateObject ) >= 0, `Unrecognized value: ${stateObject}` );
+        assert && assert( keys.includes( stateObject ), `Unrecognized value: ${stateObject}` );
         return keyToValueMap.get( stateObject )!;
       },
       stateSchema: StateSchema.asValue( `${keys.join( '|' )}`, {
@@ -53,16 +76,7 @@ const RichEnumerationIO = <T>( enumeration: any ): IOType => {
     } ) );
   }
 
-  return cache.get( enumeration );
+  return cache.get( Enumeration )!;
 };
-
-class RichEnumerationProperty<T> extends Property<T> {
-  constructor( EnumerationType: any, value: T, providedOptions?: any ) {
-    providedOptions = providedOptions || {};
-    providedOptions.validValues = _.values( EnumerationType ).filter( value => value instanceof EnumerationType );
-    providedOptions.phetioType = Property.PropertyIO( RichEnumerationIO<T>( EnumerationType ) );
-    super( value, providedOptions );
-  }
-}
 
 export default RichEnumerationProperty;
