41832b528075a8c735e2714596656faa4b917035
Sam Reid <reids@colorado.edu>
2022-01-05 16:32:13 -0700

    Remove RichEnumerationContainer parameter from RichEnumerationProperty constructor, see https://github.com/phetsims/chipper/issues/1106

--- js/RichEnumerationProperty.ts
+++ js/RichEnumerationProperty.ts
@@ -7,7 +7,6 @@
  */
 
 import Property, { PropertyOptions } from './Property.js';
-import { RichEnumerationContainer } from '../../phet-core/js/IRichEnumeration.js';
 import RichEnumerationIO from './RichEnumerationIO.js';
 import merge from '../../phet-core/js/merge.js';
 import EnumerationValue from '../../phet-core/js/EnumerationValue.js';
@@ -17,18 +16,19 @@ type RichEnumerationPropertyOptions<T> = Omit<PropertyOptions<T>, 'validValues' 
 class RichEnumerationProperty<T extends EnumerationValue> extends Property<T> {
 
   /**
-   * @param enumerationContainer - for convenience at the client site, this is an object that contains a RichEnumeration named "enum"
    * @param value
    * @param providedOptions
    */
-  constructor( enumerationContainer: RichEnumerationContainer<T>, value: T, providedOptions?: RichEnumerationPropertyOptions<T> ) {
+  constructor( value: T, providedOptions?: RichEnumerationPropertyOptions<T> ) {
 
     assert && assert( !providedOptions || !providedOptions.hasOwnProperty( 'validValues' ), 'validValues is supplied by RichEnumerationProperty' );
     assert && assert( !providedOptions || !providedOptions.hasOwnProperty( 'phetioType' ), 'phetioType is supplied by RichEnumerationProperty' );
 
     const options = merge( {}, providedOptions, {
-      validValues: enumerationContainer.enumeration.values,
-      phetioType: Property.PropertyIO( RichEnumerationIO<T>( enumerationContainer ) )
+      validValues: value.enumeration!.values,
+      phetioType: Property.PropertyIO( RichEnumerationIO<T>( {
+        enumeration: value.enumeration!
+      } ) )
     } );
 
     super( value, options );
