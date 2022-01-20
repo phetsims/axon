79498a3281759d9af7fa1e3ffbd355a14ba52f2c
zepumph <michael.kauzmann@colorado.edu>
2022-01-20 10:27:03 -0700

    add deprecation warning, https://github.com/phetsims/axon/issues/373

--- js/StringEnumerationProperty.ts
+++ js/StringEnumerationProperty.ts
@@ -2,6 +2,7 @@
 import Property, { PropertyOptions } from './Property.js';
 import StringIO from '../../tandem/js/types/StringIO.js';
 import merge from '../../phet-core/js/merge.js';
+import deprecationWarning from '../../phet-core/js/deprecationWarning.js';
 
 type StringEnumerationPropertyOptions<T> = Omit<PropertyOptions<T>, 'validValues' | 'phetioType'>;
 
@@ -9,10 +10,13 @@ type StringEnumerationPropertyOptions<T> = Omit<PropertyOptions<T>, 'validValues
  * In TypeScript, it is common to use a string literal union as an enumeration.  This type automatically specifies
  * validValues and the phetioType for convenience.
  *
+ * @deprecated
  * @author Sam Reid (PhET Interactive Simulations)
  */
 class StringEnumerationProperty<T> extends Property<T> {
   constructor( values: readonly T[], value: T, providedOptions?: StringEnumerationPropertyOptions<T> ) {
+    deprecationWarning( 'StringEnumerationProperty is soon to be deleted, instead use the EnumerationProperty in ' +
+                        'correlation with EnumerationValue subtypes (see WilderEnumerationPatterns for examples' );
 
     assert && assert( !providedOptions || !providedOptions.hasOwnProperty( 'validValues' ), 'validValues is supplied by EnumerationProperty' );
     assert && assert( !providedOptions || !providedOptions.hasOwnProperty( 'phetioType' ), 'phetioType is supplied by EnumerationProperty' );
