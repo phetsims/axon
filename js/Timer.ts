4e76561e56f68a36ef6c44c5815b7fe9d90b2049
Jonathan Olson <jonathan.olson@colorado.edu>
2022-02-23 19:04:37 -0700

    Display/Input/Pointers/SceneryEvent/Overlays/Timer => TypeScript, added Poolable2, and relevant patches based on types

--- js/Timer.ts
+++ js/Timer.ts
@@ -10,18 +10,19 @@
 import axon from './axon.js';
 import TinyEmitter from './TinyEmitter.js';
 
-class Timer extends TinyEmitter {
+type TimerListener = ( dt: number ) => void;
+
+class Timer extends TinyEmitter<[number]> {
 
   /**
    * Adds a listener to be called back once after the specified time in milliseconds
-   * @param {function} listener - called with no arguments
-   * @param {number} timeout in milliseconds
-   * @returns {(dt:number)=>void} an internally-wrapped listener which can be removed with clearTimeout
-   * @public
+   * @param listener - called with no arguments
+   * @param timeout in milliseconds
+   * @returns an internally-wrapped listener which can be removed with clearTimeout
    */
-  setTimeout( listener, timeout ) { // eslint-disable-line bad-sim-text
+  setTimeout( listener: () => void, timeout: number ): TimerListener { // eslint-disable-line bad-sim-text
     let elapsed = 0;
-    const callback = dt => {
+    const callback = ( dt: number ) => {
       elapsed += dt;
 
       // Convert seconds to ms and see if item has timed out
@@ -42,10 +43,8 @@ class Timer extends TinyEmitter {
 
   /**
    * Clear a scheduled timeout. If there was no timeout, nothing is done.
-   * @param {function} listener
-   * @public
    */
-  clearTimeout( listener ) {
+  clearTimeout( listener: TimerListener ) {
     if ( this.hasListener( listener ) ) {
       this.removeListener( listener );
     }
@@ -53,18 +52,18 @@ class Timer extends TinyEmitter {
 
   /**
    * Adds a listener to be called at specified intervals (in milliseconds)
-   * @param {function} listener - called with no arguments
-   * @param {number} interval - in milliseconds
-   * @returns {function} an internally-wrapped listener which can be removed with clearInterval
+   * @param listener - called with no arguments
+   * @param interval - in milliseconds
+   * @returns an internally-wrapped listener which can be removed with clearInterval
    * @public
    */
-  setInterval( listener, interval ) { // eslint-disable-line bad-sim-text
+  setInterval( listener: () => void, interval: number ): TimerListener { // eslint-disable-line bad-sim-text
     let elapsed = 0;
-    const callback = dt => {
+    const callback = ( dt: number ) => {
       elapsed += dt;
 
       // Convert seconds to ms and see if item has timed out
-      while ( elapsed * 1000 >= interval && this.hasListener( callback ) !== -1 ) {
+      while ( elapsed * 1000 >= interval && this.hasListener( callback ) ) {
         listener();
         elapsed = elapsed - interval / 1000.0; // Save the leftover time so it won't accumulate
       }
@@ -77,10 +76,8 @@ class Timer extends TinyEmitter {
 
   /**
    * Clear a scheduled interval. If there was no interval, nothing is done.
-   * @param {function} listener
-   * @public
    */
-  clearInterval( listener ) {
+  clearInterval( listener: TimerListener ) {
     if ( this.hasListener( listener ) ) {
       this.removeListener( listener );
     }
@@ -88,10 +85,8 @@ class Timer extends TinyEmitter {
 
   /**
    * Run a callback on the next frame. This method is largely for clarity.
-   * @public
-   * @param {function()} listener
    */
-  runOnNextTick( listener ) {
+  runOnNextTick( listener: () => void ) {
     this.setTimeout( listener, 0 );
   }
 }
