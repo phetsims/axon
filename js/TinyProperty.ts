cde0db2642ee711a9a8492cfe6a5a51fdb7e1f07
phet-dev <phetadmin@gmail.com>
2022-03-15 03:21:50 -0600

    update copyright dates from daily grunt work

--- js/TinyProperty.ts
+++ js/TinyProperty.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * A lightweight version of Property (that satisfies some of the interface), meant for high-performance applications
