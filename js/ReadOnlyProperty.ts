b5c62d90476a1e9c2684bbb71ffa6183b5c775a5
Michael Kauzmann <michael.kauzmann@colorado.edu>
2022-08-30 17:11:07 -0600

    formatting, https://github.com/phetsims/axon/issues/409

--- js/ReadOnlyProperty.ts
+++ js/ReadOnlyProperty.ts
@@ -230,10 +230,10 @@ export default class ReadOnlyProperty<T> extends PhetioObject implements TReadOn
     // state is managed by the PhetioStateEngine
     const setManagedByPhetioState = _.hasIn( window, 'phet.joist.sim.isSettingPhetioStateProperty' ) &&
                                     phet.joist.sim.isSettingPhetioStateProperty.value &&
-                                    this.isPhetioInstrumented() && this.phetioState
+                                    this.isPhetioInstrumented() && this.phetioState &&
 
                                     // However, DerivedProperty should be able to update during PhET-iO state set
-                                    && this.isSettable();
+                                    this.isSettable();
 
     if ( !setManagedByPhetioState ) {
       this.unguardedSet( value );
