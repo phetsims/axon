c86188afd83aaf462d92a69b780ab02c446966a9
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-01-05 12:15:51 -0700

    Use RangedProperty for better typescript implementation. https://github.com/phetsims/axon/issues/425

--- js/UnitConversionProperty.ts
+++ js/UnitConversionProperty.ts
@@ -43,10 +43,10 @@ export default class UnitConversionProperty extends MappedProperty<number, numbe
 
   public readonly rangeProperty: TProperty<Range>;
 
-  private _property: TProperty<number>;
+  private _property: RangedProperty;
   private _rangeListener?: ( range: Range ) => void;
 
-  public constructor( property: TProperty<number>, providedOptions: UnitConversionPropertyOptions ) {
+  public constructor( property: RangedProperty, providedOptions: UnitConversionPropertyOptions ) {
 
     const map = ( input: number ) => input * providedOptions.factor;
     const inverseMap = ( output: number ) => output / providedOptions.factor;
@@ -67,15 +67,13 @@ export default class UnitConversionProperty extends MappedProperty<number, numbe
     this.rangeProperty = new Property<Range>( DEFAULT_RANGE );
 
     // Watch the range of the target Property, and update ours to match
-    if ( ( property as NumberProperty ).rangeProperty ) {
-      this._rangeListener = ( range: Range ) => {
-        const min = map( range.min );
-        const max = map( range.max );
-        // Handle a negative factor or something else where the min/max gets swapped
-        this.rangeProperty.value = new Range( Math.min( min, max ), Math.max( min, max ) );
-      };
-      ( property as NumberProperty ).rangeProperty.link( this._rangeListener );
-    }
+    this._rangeListener = ( range: Range ) => {
+      const min = map( range.min );
+      const max = map( range.max );
+      // Handle a negative factor or something else where the min/max gets swapped
+      this.rangeProperty.value = new Range( Math.min( min, max ), Math.max( min, max ) );
+    };
+    property.rangeProperty.link( this._rangeListener );
   }
 
   public get range(): Range {
