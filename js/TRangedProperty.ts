514e6d9318995d7de605c77402ac03b2fafc2be0
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-01-05 13:00:29 -0700

    use isTRangedProperty to allow UnitConversionProperty to access non-ranged Properties, https://github.com/phetsims/axon/issues/425

--- js/TRangedProperty.ts
+++ js/TRangedProperty.ts
@@ -3,8 +3,18 @@
 import LinkableProperty from './LinkableProperty.js';
 import TReadOnlyProperty from './TReadOnlyProperty.js';
 import Range from '../../dot/js/Range.js';
+import IntentionalAny from '../../phet-core/js/types/IntentionalAny.js';
+import ReadOnlyProperty from './ReadOnlyProperty.js';
+import TinyProperty from './TinyProperty.js';
 
 // Minimal types for Properties that support a rangeProperty.
 export type TRangedProperty = LinkableProperty<number> & { range: Range; readonly rangeProperty: TReadOnlyProperty<Range> };
+
+export function isTRangedProperty( something: IntentionalAny ): something is TRangedProperty {
+  return ( something instanceof ReadOnlyProperty || something instanceof TinyProperty ) && something.isSettable() &&
+
+         // @ts-expect-error we are checking on the pressence, but can't use hasOwnProperty in case it is implemented wil es5 getters and setters
+         !!something.range && !!something.rangeProperty;
+}
 
 export default TRangedProperty;
--- js/UnitConversionProperty.ts
+++ js/UnitConversionProperty.ts
@@ -27,9 +27,9 @@ import Range from '../../dot/js/Range.js';
 import TProperty from './TProperty.js';
 import MappedProperty, { MappedPropertyOptions } from './MappedProperty.js';
 import Property from './Property.js';
-import NumberProperty, { DEFAULT_RANGE } from './NumberProperty.js';
+import { DEFAULT_RANGE } from './NumberProperty.js';
 import optionize from '../../phet-core/js/optionize.js';
-import TRangedProperty from './TRangedProperty.js';
+import TRangedProperty, { isTRangedProperty } from './TRangedProperty.js';
 
 type SelfOptions = {
   // The multiplicative factor to convert from INPUT => OUTPUT, e.g.
@@ -44,10 +44,10 @@ export default class UnitConversionProperty extends MappedProperty<number, numbe
 
   public readonly rangeProperty: TProperty<Range>;
 
-  private _property: TRangedProperty;
+  private _property: ( TProperty<number> | TRangedProperty );
   private _rangeListener?: ( range: Range ) => void;
 
-  public constructor( property: TRangedProperty, providedOptions: UnitConversionPropertyOptions ) {
+  public constructor( property: ( TProperty<number> | TRangedProperty ), providedOptions: UnitConversionPropertyOptions ) {
 
     const map = ( input: number ) => input * providedOptions.factor;
     const inverseMap = ( output: number ) => output / providedOptions.factor;
@@ -67,14 +67,17 @@ export default class UnitConversionProperty extends MappedProperty<number, numbe
 
     this.rangeProperty = new Property<Range>( DEFAULT_RANGE );
 
-    // Watch the range of the target Property, and update ours to match
-    this._rangeListener = ( range: Range ) => {
-      const min = map( range.min );
-      const max = map( range.max );
-      // Handle a negative factor or something else where the min/max gets swapped
-      this.rangeProperty.value = new Range( Math.min( min, max ), Math.max( min, max ) );
-    };
-    property.rangeProperty.link( this._rangeListener );
+    if ( isTRangedProperty( property ) ) {
+
+      // Watch the range of the target Property, and update ours to match
+      this._rangeListener = ( range: Range ) => {
+        const min = map( range.min );
+        const max = map( range.max );
+        // Handle a negative factor or something else where the min/max gets swapped
+        this.rangeProperty.value = new Range( Math.min( min, max ), Math.max( min, max ) );
+      };
+      property.rangeProperty.link( this._rangeListener );
+    }
   }
 
   public get range(): Range {
@@ -87,8 +90,8 @@ export default class UnitConversionProperty extends MappedProperty<number, numbe
   }
 
   public override dispose(): void {
-    if ( ( this._property as NumberProperty ).rangeProperty ) {
-      ( this._property as NumberProperty ).rangeProperty.unlink( this._rangeListener! );
+    if ( isTRangedProperty( this._property ) ) {
+      this._property.rangeProperty.unlink( this._rangeListener! );
     }
 
     this.rangeProperty.dispose();
