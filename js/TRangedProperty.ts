293217b0764464c97b3d3aa1bbe9dbba7693cdbf
phet-dev <phetadmin@gmail.com>
2023-01-06 03:58:06 -0700

    update copyright dates from daily grunt work

--- js/TRangedProperty.ts
+++ js/TRangedProperty.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2023, University of Colorado Boulder
 
 import LinkableProperty from './LinkableProperty.js';
 import TReadOnlyProperty from './TReadOnlyProperty.js';
