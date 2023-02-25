8a60f1a6542dc488571f6531dc020257a18ce84c
phet-dev <phetadmin@gmail.com>
2023-02-25 04:15:09 -0700

    update copyright dates from daily grunt work

--- js/Disposable.ts
+++ js/Disposable.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * Type to implement disposal strategies, with a disposer option and a disposeEmitter.
