95e6058546cc5e330da2b9ef136220ab00d5ab84
Sam Reid <reids@colorado.edu>
2023-04-23 21:11:03 -0600

    Only show range readout when range selected, see https://github.com/phetsims/center-and-variability/issues/149

--- js/DerivedProperty.ts
+++ js/DerivedProperty.ts
@@ -197,8 +197,12 @@ export default class DerivedProperty<T, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10,
    * Creates a derived boolean Property whose value is true iff firstProperty's value is equal to secondProperty's
    * value.
    */
-  private static valueEquals( firstProperty: TReadOnlyProperty<unknown>, secondProperty: TReadOnlyProperty<unknown>, options?: DerivedPropertyOptions<boolean> ): TReadOnlyProperty<boolean> {
+  public static valueEquals( firstProperty: TReadOnlyProperty<unknown>, secondProperty: TReadOnlyProperty<unknown>, options?: DerivedPropertyOptions<boolean> ): TReadOnlyProperty<boolean> {
     return new DerivedProperty( [ firstProperty, secondProperty ], ( u: unknown, v: unknown ) => u === v, options );
+  }
+
+  public static valueEqualsConstant( firstProperty: TReadOnlyProperty<unknown>, value: unknown, options?: DerivedPropertyOptions<boolean> ): TReadOnlyProperty<boolean> {
+    return new DerivedProperty( [ firstProperty ], ( u: unknown ) => u === value, options );
   }
 
   /**
