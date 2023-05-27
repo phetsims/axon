9abe66d49ab1670ea2f45b9b6006782550eee861
phet-dev <phetadmin@gmail.com>
2023-05-27 03:25:38 -0600

    update copyright dates from daily grunt work

--- js/units.ts
+++ js/units.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2018-2023, University of Colorado Boulder
 
 /**
  * These are the units that can be associated with Property instances.
