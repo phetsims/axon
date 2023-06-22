1d7f7b4bfbec4aebcebbf0eff34eafb02f8e9de7
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-22 17:32:29 -0600

    convert to Disposable.assertNotDisposable(), https://github.com/phetsims/axon/issues/436

--- js/Disposable.ts
+++ js/Disposable.ts
@@ -88,6 +88,8 @@ class Disposable {
   }
 
   public static assertNotDisposable(): void {
+
+    // eslint-disable-next-line bad-sim-text
     assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
   }
 }
