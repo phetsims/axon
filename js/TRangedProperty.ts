96745eaf3f43485e19f32ca941a0f5add2ef4911
phet-dev <phetadmin@gmail.com>
2024-01-06 03:10:29 -0700

    update copyright dates from daily grunt work

--- js/TRangedProperty.ts
+++ js/TRangedProperty.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 // @author Michael Kauzmann (PhET Interactive Simulations)
 
 import TReadOnlyProperty from './TReadOnlyProperty.js';
--- js/animationFrameTimer.ts
+++ js/animationFrameTimer.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, University of Colorado Boulder
+// Copyright 2020-2024, University of Colorado Boulder
 // @author Sam Reid (PhET Interactive Simulations)
 
 import axon from './axon.js';
