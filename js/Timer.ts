e6cb78a25e64e9732eb0bd5a4c051db56a3a54ff
phet-dev <phetadmin@gmail.com>
2024-03-05 03:12:01 -0700

    update copyright dates from daily grunt work

--- js/Timer.ts
+++ js/Timer.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2023, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * Timer so that other modules can run timing related code through the simulation's requestAnimationFrame. Use its
