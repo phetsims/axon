e8262d04a9517c7eaa4ec4950c670200c69817fe
Sam Reid <reids@colorado.edu>
2024-03-06 14:12:53 -0700

    Factor out TReadOnlyEmitter, see https://github.com/phetsims/axon/issues/450

--- js/TEmitter.ts
+++ js/TEmitter.ts
@@ -9,28 +9,28 @@
  */
 
 import IntentionalAny from '../../phet-core/js/types/IntentionalAny.js';
-import StrictOmit from '../../phet-core/js/types/StrictOmit.js';
 
 // undefined and never are not allowed as parameters to Emitter
 export type TEmitterParameter = Exclude<IntentionalAny, undefined | never>;
 
 export type TEmitterListener<T extends TEmitterParameter[]> = ( ...args: T ) => void;
 
-type TEmitter<T extends TEmitterParameter[] = []> = {
-
-  // For all the methods, please see documentation in Emitter.ts
-  emit: ( ...args: T ) => void;
+// A "read only" version of an emitter. You can listen to it, but cannot control it
+export type TReadOnlyEmitter<T extends TEmitterParameter[] = []> = {
 
   addListener: ( listener: TEmitterListener<T> ) => void;
   hasListener: ( listener: TEmitterListener<T> ) => boolean;
 
   removeListener: ( listener: TEmitterListener<T> ) => void;
   removeAllListeners: () => void;
+};
+
+type TEmitter<T extends TEmitterParameter[] = []> = TReadOnlyEmitter<T> & {
+
+  // For all the methods, please see documentation in Emitter.ts
+  emit: ( ...args: T ) => void;
 
   dispose: () => void;
 };
-
-// A "read only" version of an emitter. You can listen to it, but cannot control it
-export type TReadOnlyEmitter<T extends TEmitterParameter[] = []> = StrictOmit<TEmitter<T>, 'emit' | 'dispose'>;
 
 export default TEmitter;
