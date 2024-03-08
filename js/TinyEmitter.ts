8a25946d4d56d1339b682c67216f295f9bcd8ba6
phet-dev <phetadmin@gmail.com>
2024-03-08 03:12:02 -0700

    update copyright dates from daily grunt work

--- js/TinyEmitter.ts
+++ js/TinyEmitter.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2023, University of Colorado Boulder
+// Copyright 2015-2024, University of Colorado Boulder
 
 /**
  * Lightweight event & listener abstraction for a single event type.
