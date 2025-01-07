56bc94890d8a47e2a1f629e33a504212e2bd5c37
phet-dev <phetadmin@gmail.com>
2025-01-07 03:17:50 -0700

    update copyright dates from daily grunt work

--- js/Emitter.ts
+++ js/Emitter.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2024, University of Colorado Boulder
+// Copyright 2019-2025, University of Colorado Boulder
 
 /**
  * Event & listener abstraction for a single "event" type. The type provides extra functionality beyond just notifying
--- js/TinyEmitter.ts
+++ js/TinyEmitter.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2024, University of Colorado Boulder
+// Copyright 2015-2025, University of Colorado Boulder
 
 /**
  * Lightweight event & listener abstraction for a single event type.
