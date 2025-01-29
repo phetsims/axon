83c03a9e499869c4c3906f2c8059b6704397ecef
phet-dev <phetadmin@gmail.com>
2025-01-29 03:26:08 -0700

    update copyright dates from daily grunt work

--- js/createObservableArray.ts
+++ js/createObservableArray.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2024, University of Colorado Boulder
+// Copyright 2020-2025, University of Colorado Boulder
 
 // createObservableArray conforms to the Proxy interface, which is polluted with `any` types.  Therefore we disable
 // this rule for this file.
