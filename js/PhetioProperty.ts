bb211c11ac76388a96b99c0d63014478d91d73fb
Sam Reid <reids@colorado.edu>
2025-07-09 07:01:49 -0600

    Format documentation, see https://github.com/phetsims/chipper/issues/1608

--- js/PhetioProperty.ts
+++ js/PhetioProperty.ts
@@ -1,13 +1,19 @@
 // Copyright 2023-2025, University of Colorado Boulder
-// @author Michael Kauzmann (PhET Interactive Simulations)
+
+/**
+ * When calling PhetioObject.addLinkedElement, the parameter must be a PhetioObject, so this type alias covers that ground
+ * and helps with cases like LocalizedStringProperty (and DynamicProperty in general) which satisfies the TProperty
+ * interface and is a PhetioObject. It is best to try to use the ReadOnlyProperty class hierarchy first, and this Type
+ * Alias can help cover cases where that doesn't work, see https://github.com/phetsims/tandem/issues/299
+ *
+ * See also PhetioReadOnlyProperty, which is used for writable properties.
+ *
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
 
 import type PhetioObject from '../../tandem/js/PhetioObject.js';
 import type TProperty from './TProperty.js';
 
-// When calling PhetioObject.addLinkedElement, the parameter must be a PhetioObject, so this type alias covers that ground
-// and helps with cases like LocalizedStringProperty (and DynamicProperty in general) which satisfies the TProperty
-// interface and is a PhetioObject. It is best to try to use the ReadOnlyProperty class hierarchy first, and this Type
-// Alias can help cover cases where that doesn't work, see https://github.com/phetsims/tandem/issues/299
 type PhetioProperty<T> = TProperty<T> & PhetioObject;
 
 export default PhetioProperty;
