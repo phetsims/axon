21691915574dddf8530dc35079d98cd8c273c8c4
Sam Reid <reids@colorado.edu>
2025-10-23 16:09:46 -0600

    Add derivedMap, see https://github.com/phetsims/number-pairs/issues/307

---
+++ js/derivedMap.ts
@@ -0,0 +1,45 @@
+// Copyright 2025, University of Colorado Boulder
+
+/**
+ * Generalized convenience for deriving a Property from a key Property and a Map of cases.
+ * Works with ANY key type (booleans, object singletons, etc.). When `fallback` is provided,
+ * it is used if the key isn't present in `map`.
+ *
+ * Map values may be plain values or TReadOnlyProperty values; all property-valued entries
+ * (plus an optional fallback property) are tracked as dependencies so updates propagate.
+ *
+ * See also:
+ *  - DerivedProperty.fromRecord for string/number/symbol keys
+ *  - derivedTernary for the boolean-literal object-literal convenience
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+import affirm from '../../perennial-alias/js/browser-and-node/affirm.js';
+import DerivedProperty, { type UnknownDerivedProperty } from './DerivedProperty.js';
+import { isTReadOnlyProperty, TReadOnlyProperty } from './TReadOnlyProperty.js';
+
+// Overloads for better DX
+function derivedMap<K, V>(
+  key: TReadOnlyProperty<K>,
+  map: ReadonlyMap<K, TReadOnlyProperty<V> | V>
+): UnknownDerivedProperty<V> {
+
+  // All the dependencies that are Properties
+  const m: TReadOnlyProperty<V>[] = Array.from( map.values() ).filter( value => isTReadOnlyProperty<V>( value ) );
+
+  return DerivedProperty.deriveAny( [ key, ...m ], () => {
+
+    affirm( map.has( key.value ), `derivedMap: key not found in map: ${key.value}` );
+    const value = map.get( key.value );
+
+    if ( isTReadOnlyProperty( value ) ) {
+      return value.value;
+    }
+    else {
+      return value as V;
+    }
+  } );
+}
+
+export default derivedMap;
