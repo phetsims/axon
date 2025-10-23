4ce79b0b554cda85cefe8d7fa9b48a4190c6be34
Sam Reid <reids@colorado.edu>
2025-10-23 15:36:18 -0600

    Use derivedTernary, see https://github.com/phetsims/number-pairs/issues/307

--- js/derivedTernary.ts
+++ js/derivedTernary.ts
@@ -9,12 +9,30 @@
 
 import derived from './derived.js';
 import { type UnknownDerivedProperty } from './DerivedProperty.js';
-import type { TReadOnlyProperty } from './TReadOnlyProperty.js';
+import { isTReadOnlyProperty, TReadOnlyProperty } from './TReadOnlyProperty.js';
 
-function derivedTernary<T, V>( predicateProperty: TReadOnlyProperty<boolean>, map: { true: TReadOnlyProperty<T>; false: TReadOnlyProperty<V> } ): UnknownDerivedProperty<T | V> {
-  return derived( predicateProperty, map.true, map.false, ( predicate, trueValue, falseValue ) => {
-    return predicate ? trueValue : falseValue;
-  } );
+function derivedTernary<T, V>( predicateProperty: TReadOnlyProperty<boolean>, map: { true: TReadOnlyProperty<T> | T; false: TReadOnlyProperty<V> | V } ): UnknownDerivedProperty<T | V> {
+
+  if ( isTReadOnlyProperty( map.true ) && isTReadOnlyProperty( map.false ) ) {
+    return derived( predicateProperty, map.true, map.false, ( predicate, trueValue, falseValue ) => {
+      return predicate ? trueValue : falseValue;
+    } );
+  }
+  else if ( isTReadOnlyProperty( map.true ) ) {
+    return derived( predicateProperty, map.true, ( predicate, trueValue ) => {
+      return predicate ? trueValue : map.false as V;
+    } );
+  }
+  else if ( isTReadOnlyProperty( map.false ) ) {
+    return derived( predicateProperty, map.false, ( predicate, falseValue ) => {
+      return predicate ? map.true as T : falseValue;
+    } );
+  }
+  else {
+    return derived( predicateProperty, predicate => {
+      return predicate ? map.true as T : map.false as V;
+    } );
+  }
 }
 
 export default derivedTernary;
