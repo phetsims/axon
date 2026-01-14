22435d9bf62f54a2a917ab3adfc4c8db1c433aeb
Sam Reid <reids@colorado.edu>
2026-01-14 06:04:56 -0700

    Convert to TypeScript, see https://github.com/phetsims/axon/issues/469

--- js/EnumerationDeprecatedProperty.ts
+++ js/EnumerationDeprecatedProperty.ts
@@ -9,6 +9,7 @@
 import deprecationWarning from '../../phet-core/js/deprecationWarning.js';
 import EnumerationDeprecated from '../../phet-core/js/EnumerationDeprecated.js';
 import merge from '../../phet-core/js/merge.js';
+import IntentionalAny from '../../phet-core/js/types/IntentionalAny.js';
 import EnumerationIO from '../../tandem/js/types/EnumerationIO.js';
 import axon from './axon.js';
 import Property from './Property.js';
@@ -16,17 +17,17 @@ import Property from './Property.js';
 /**
  * @deprecated
  */
-class EnumerationDeprecatedProperty extends Property {
+class EnumerationDeprecatedProperty extends Property<object> {
 
   /**
-   * @param {EnumerationDeprecated | any} enumeration
-   * @param {*} initialValue - one of the values from enumeration
-   * @param {Object} [options]
+   * @param enumeration
+   * @param initialValue - one of the values from enumeration
+   * @param [options]
    */
-  constructor( enumeration, initialValue, options ) {
+  public constructor( public readonly enumeration: EnumerationDeprecated, initialValue: object, options?: IntentionalAny ) {
     deprecationWarning( 'Use EnumerationProperty. EnumerationDeprecated should be exchanged for classes that extend EnumerationValue, see WilderEnumerationPatterns for examples.' );
 
-    assert && assert( enumeration instanceof EnumerationDeprecated, 'likely you are using the new and improved Enumeration, better use EnumerationProperty too.' );
+    // eslint-disable-next-line @typescript-eslint/no-base-to-string
     assert && assert( enumeration.VALUES.includes( initialValue ), `invalid initialValue: ${initialValue}` );
 
     if ( options ) {
@@ -39,16 +40,16 @@ class EnumerationDeprecatedProperty extends Property {
       assert && assert( !options.hasOwnProperty( 'phetioType' ), 'EnumerationDeprecatedProperty sets phetioType' );
     }
 
+    // eslint-disable-next-line phet/bad-typescript-text
     options = merge( {
       valueType: enumeration,
+
+      // @ts-expect-error
       phetioValueType: EnumerationIO( enumeration ),
       validValues: enumeration.VALUES // for PhET-iO documentation and support
     }, options );
 
     super( initialValue, options );
-
-    // @public (read-only)
-    this.enumeration = enumeration;
   }
 }
 
