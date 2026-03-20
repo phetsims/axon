600ab254acf4c934994d7af8a4847c89a2166df5
Sam Reid <reids@colorado.edu>
2026-03-20 08:07:13 -0600

    Revert "update copyright dates from daily grunt work"
    This reverts commit 0f614bf8f3f256fa4df80accc9d14467d72c4c78.

--- js/animationFrameTimer.ts
+++ js/animationFrameTimer.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2026, University of Colorado Boulder
+// Copyright 2020-2024, University of Colorado Boulder
 // @author Sam Reid (PhET Interactive Simulations)
 
 import Timer from './Timer.js';
